53314a1bddcb190b8deef28292756bba913fee68
skybreakbb <willistaylora@gmail.com>
2023-09-10 19:52:55 -0500

    added images for cards in Portfolio.jsx, added conditional rendering of links to deployed application

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,43 @@
 import PropTypes from 'prop-types';
+import { styled } from 'styled-components';
+import GitHubLogo from '../assets/GitHubLogo.png';
+import ExternalLinkLogo from '../assets/ExternalLinkLogo.png'
+
+const StyledImg = styled.img`
+`
+const StyledDiv = styled.div`
+opacity: 0%;
+transition: 0.3s;
+&:hover {
+  opacity: 100%;
+  background-color: #ffffff7b;
+  transition: 0.3s;
+}
+
+`
 
 export default function Project(props) {
   return (
     <>
     <div className="container col-lg-6 col-md-12 flex-md-fill pb-4">
-      <div className="card bg-dark text-white">
-        <img src={props.srcUrl} className="card-img" alt="{props.description}" />
-        <div className="card-img-overlay">
-          <h5 className="card-title">{props.title}</h5>
+      <div className="card bg-dark text-black">
+        <StyledImg src={props.srcUrl} className="card-img" alt="{props.description}" />
+        <StyledDiv className="card-img-overlay">
+          <h4 className="card-title">{props.title}</h4>
           <p className="card-text">
             {props.description}
           </p>
-          <p className="card-text">test</p>
-        </div>
+          <div className="d-flex gap-4">
+          <a href={props.gitLink} target="_blank" rel="noreferrer">
+            <img src={GitHubLogo} height="35px" />
+          </a>
+          {props.deployedLink !== "N/A" && 
+          <a href={props.deployedLink} target="_blank" rel="noreferrer">
+            <img src={ExternalLinkLogo} height="35px" />
+          </a>
+          }
+          </div>
+        </StyledDiv>
       </div>
     </div>
     </>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,68 +1,69 @@
 import Project from "../components/Project";
+import portfolioImage1 from "../assets/portfolioImage1.png";
+import portfolioImage2 from "../assets/portfolioImage2.png";
+import portfolioImage3 from "../assets/portfolioImage3.png";
+import portfolioImage4 from "../assets/portfolioImage4.png";
+import portfolioImage5 from "../assets/portfolioImage5.png";
+import portfolioImage6 from "../assets/portfolioImage6.png";
+import portfolioImage7 from "../assets/portfolioImage7.png";
+import portfolioImage8 from "../assets/portfolioImage8.png";
 
 export default function Portfolio() {
   const projects = [
     {
-      srcUrl: "https://placehold.co/426x240",
-      title: "Project 1",
+      srcUrl: portfolioImage1,
+      title: "Maintenance Manatee",
+      description: "this is a test description",
+      deployedLink: "https://maintenance-manatee-20230807-e071f072b462.herokuapp.com/",
+      gitLink: "https://github.com/cdgonzo23/maintenance-manatee",
+    },
+    {
+      srcUrl: portfolioImage2,
+      title: "SVG Logo Generator",
+      description: "this is a test description",
+      deployedLink: "N/A",
+      gitLink: "test-git-link",
+    },
+    {
+      srcUrl: portfolioImage3,
+      title: "Weather Dashboard",
       description: "this is a test description",
       deployedLink: "test-deployed-link",
       gitLink: "test-git-link",
     },
     {
-      srcUrl: "https://placehold.co/426x240",
-      title: "Project 2",
+      srcUrl: portfolioImage4,
+      title: "JavaScript-based Quiz",
       description: "this is a test description",
       deployedLink: "test-deployed-link",
       gitLink: "test-git-link",
     },
     {
-      srcUrl: "https://placehold.co/426x240",
-      title: "Project 3",
+      srcUrl: portfolioImage5,
+      title: "HTML/CSS Project Portfolio",
       description: "this is a test description",
       deployedLink: "test-deployed-link",
       gitLink: "test-git-link",
     },
     {
-      srcUrl: "https://placehold.co/426x240",
-      title: "Project 4",
+      srcUrl: portfolioImage6,
+      title: "Employee Tracker API",
+      description: "this is a test description",
+      deployedLink: "N/A",
+      gitLink: "test-git-link",
+    },
+    {
+      srcUrl: portfolioImage7,
+      title: "CMS Style Blog",
       description: "this is a test description",
       deployedLink: "test-deployed-link",
       gitLink: "test-git-link",
     },
     {
-      srcUrl: "https://placehold.co/426x240",
-      title: "Project 5",
+      srcUrl: portfolioImage8,
+      title: "Social Media API",
       description: "this is a test description",
-      deployedLink: "test-deployed-link",
-      gitLink: "test-git-link",
-    },
-    {
-      srcUrl: "https://placehold.co/426x240",
-      title: "Project 6",
-      description: "this is a test description",
-      deployedLink: "test-deployed-link",
-      gitLink: "test-git-link",
-    },
-    {
-      srcUrl: "https://placehold.co/426x240",
-      title: "Project 7",
-      description: "this is a test description",
-      deployedLink: "test-deployed-link",
-      gitLink: "test-git-link",
-    },
-    {
-      srcUrl: "https://placehold.co/426x240",
-      title: "Project 8",
-      description: "this is a test description",
-      deployedLink: "test-deployed-link",
-      gitLink: "test-git-link",
-    },
-    {
-      srcUrl: "https://placehold.co/426x240",
-      title: "Project 9",
-      description: "this is a test description",
-      deployedLink: "test-deployed-link",
+      deployedLink: "N/A",
       gitLink: "test-git-link",
     },
   ];
