7c59f905c6495ecc34bf93440a7cf4f72e2932ee
skybreakbb <willistaylora@gmail.com>
2023-09-14 18:10:48 -0500

    updated 'About' page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,67 +1,32 @@
+import aboutImage from "../assets/aboutImage1.jpg";
+
 export default function About() {
   return (
-    <div className="container text-center p-4">
-      <div className="row justify-content-center">
-        <div className="col-lg-8 col-sm-6">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-          dolor sit, amet consectetur adipisicing elit. Laudantium laboriosam,
-          quisquam dolore unde temporibus nihil error neque maxime nemo sunt ab
-          facere nulla illo est officia voluptates officiis fugit at. Lorem,
-          ipsum dolor sit amet consectetur adipisicing elit. Dolorum, id
-          asperiores blanditiis maxime maiores veniam repudiandae nesciunt, non
-          tempora rerum perspiciatis doloribus quibusdam, perferendis fuga hic
-          dolore necessitatibus? Odit, quidem!
-          
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-          dolor sit, amet consectetur adipisicing elit. Laudantium laboriosam,
-          quisquam dolore unde temporibus nihil error neque maxime nemo sunt ab
-          facere nulla illo est officia voluptates officiis fugit at. Lorem,
-          ipsum dolor sit amet consectetur adipisicing elit. Dolorum, id
-          asperiores blanditiis maxime maiores veniam repudiandae nesciunt, non
-          tempora rerum perspiciatis doloribus quibusdam, perferendis fuga hic
-          dolore necessitatibus? Odit, quidem!
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-          dolor sit, amet consectetur adipisicing elit. Laudantium laboriosam,
-          quisquam dolore unde temporibus nihil error neque maxime nemo sunt ab
-          facere nulla illo est officia voluptates officiis fugit at. Lorem,
-          ipsum dolor sit amet consectetur adipisicing elit. Dolorum, id
-          asperiores blanditiis maxime maiores veniam repudiandae nesciunt, non
-          tempora rerum perspiciatis doloribus quibusdam, perferendis fuga hic
-          dolore necessitatibus? Odit, quidem!Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-          dolor sit, amet consectetur adipisicing elit. Laudantium laboriosam,
-          quisquam dolore unde temporibus nihil error neque maxime nemo sunt ab
-          facere nulla illo est officia voluptates officiis fugit at. Lorem,
-          ipsum dolor sit amet consectetur adipisicing elit. Dolorum, id
-          asperiores blanditiis maxime maiores veniam repudiandae nesciunt, non
-          tempora rerum perspiciatis doloribus quibusdam, perferendis fuga hic
-          dolore necessitatibus? Odit, quidem!
+    <div className="container p-4">
+      <div className="row text-center justify-content-center">
+        <div className="col-lg-3 pb-4 order-md-1">
+          <img src={aboutImage} height="400px"/>
         </div>
-        <div className="col-lg-3 col-sm-6">
-          <img src="https://placehold.co/250x300" />
+        <div className="col-lg-7 mx-4 order-md-2">
+          <h5 className="pb-2">
+            Hi, my name is Taylor and I&#39;m a web developer.
+          </h5>
+          <p className="pb-2">
+            I&#39;ve always enjoyed learning new things, solving problems, as
+            well as creating things from scratch. Web development satisfies all
+            of those criteria quite well! In fact, this portfolio is my first
+            attempt at building a React application from scratch and certainly
+            won&#39;t be my last.
+          </p>
+          <p className="pb-2">
+            In my time learning full-stack web development I&#39;ve become quite
+            proficient in several front-end and back-end disciplines with a relatively heavy focus on best practices and various other methodologies.
+          </p>
+          <p className="pb-2">
+            I&#39;m currently enrolled in, and nearing completion of, Vanderbilt
+            University&#39;s Full Stack Web Development bootcamp.
+          </p>
+          <p></p>
         </div>
       </div>
     </div>
